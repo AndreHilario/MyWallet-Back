c310b5c91aa073f898d66e40ffd9bcc67dfcbe1e
Andre Hilario <andre18hilario@gmail.com>
2023-04-21 12:03:41 -0300

    Route-delete: /home/:id OK!

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { db } from "../database/database.connection.js";
 import dayjs from "dayjs";
 
@@ -14,7 +15,7 @@ export async function setTransaction(req, res) {
 
         const correctBody = { ...req.body, status: tipo, date: formattedDate, idUser: session.idUser };
         await db.collection("transactions").insertOne(correctBody);
-        
+
         res.sendStatus(201);
 
     } catch (err) {
@@ -35,3 +36,19 @@ export async function getTransactions(req, res) {
         res.status(500).send(err.message);
     }
 };
+
+export async function deleteTransaction(req, res) {
+
+    const { id } = req.params;
+
+    try {
+
+        const deleteResult = await db.collection("transactions").deleteOne({_id: new ObjectId(id)});
+        if(deleteResult.deletedCount === 0) return res.status(404).send("Transaction not found");
+
+        res.sendStatus(204);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getTransactions, setTransaction } from "../controllers/transactions.controller.js";
+import { deleteTransaction, getTransactions, setTransaction } from "../controllers/transactions.controller.js";
 import { validateSchema, validateSchemaParams } from "../middlewares/validateSchema.middleware.js";
 import { numberSchema } from "../schemas/number.schema.js";
 import { authValidation } from "../middlewares/auth.middleware.js";
 import { paramsSchema } from "../schemas/params.schema.js";
+import { paramsIdSchema } from "../schemas/id.schema.js";
 
 const transactionRouter = Router();
 
@@ -12,5 +13,7 @@ transactionRouter.use(authValidation);
 transactionRouter.post("/nova-transacao/:tipo", validateSchemaParams(paramsSchema), validateSchema(numberSchema), setTransaction);
 
 transactionRouter.get("/home", getTransactions);
+
+transactionRouter.delete("/home/:id", validateSchemaParams(paramsIdSchema), deleteTransaction);
 
 export default transactionRouter;
